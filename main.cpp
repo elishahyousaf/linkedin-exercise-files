4ee789d70a3fe7d8d5e4248bbc9f4d05fd0cc563
Elishah Yousaf <elishahroy@gmail.com>
2021-04-13 21:54:56 +0500

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
                               Online C++ Compiler.
                Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+Write your code in this editor and press "Run" buthhton to compile and execute it.
 
 
 *******************************************************************************/
